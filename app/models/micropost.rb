edfc3303100fb2636be1a53d46adfcca35faa52f
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-21 10:14:06 +1000

    Mircorpost and user validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,4 @@
 class Micropost < ApplicationRecord
+  belong_to :user
+  validates :content, length: { maximum: 140 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts
+  validates FILL_IN, presence: true #replace fill with correct placeholder
+  validates FILL_IN, presence: true #replace fill_in with the correct code
 end
