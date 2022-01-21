54857e85d43039d6c6ff0fde96468dc2cbbacb45
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-21 13:51:06 +1000

    Delete validation code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
   has_many :microposts
-  validates FILL_IN, presence: true #replace fill with correct placeholder
-  validates FILL_IN, presence: true #replace fill_in with the correct code
+
 end
