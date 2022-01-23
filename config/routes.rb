cc3766b9c0da8c49f795bbdcd8881260dd4af60d
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-24 08:32:13 +1000

    Add functional links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/contact'
+  root           'static_pages#home'
+  get 'about'   => 'static_pages#about'
+  get 'help'    => 'static_pages#help'
+  get 'contact' => 'static_pages#contact'
   resources :microposts
   resources :users
-  get 'static_pages/home'
-  root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
