e564d55ce836f8a31b4fe7d8955e478c6428cd0b
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-24 09:07:32 +1000

    Mae timesteamps false

--- db/migrate/20220120055151_create_users.rb
+++ db/migrate/20220120055151_create_users.rb
@@ -1,10 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
+    #Type and attributes. T is an attribute passed to block. T = table
     create_table :users do |t|
       t.string :name
       t.string :email
-
-      t.timestamps
+      #Tell db that it should not be false
+      t.timestamps null: false
     end
   end
 end
