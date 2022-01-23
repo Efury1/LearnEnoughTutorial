1d35e66c0e3e12c0e37aedc2e803f2a1e8ce1a04
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-24 07:52:05 +1000

    Organise pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
   resources :microposts
   resources :users
-  get 'welcome/homepage'
-  root 'welcome#homepage'
+  get 'static_pages/home'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
