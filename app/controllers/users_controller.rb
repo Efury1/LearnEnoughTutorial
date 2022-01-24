36cf7e99ef99501d45e3ccd3f7883ec060cc9c7d
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-25 09:07:38 +1000

    Make sign up page button work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  # before_action :set_user, only: %i[ show edit update destroy ]
 
   # GET /users or /users.json
   def index
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      redirect_to @user
     else
       render 'new'
     end
@@ -37,38 +37,39 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-
-  # PATCH/PUT /users/1 or /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1 or /users/1.json
-  def destroy
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :email)
-    end
 end
+
+#   # PATCH/PUT /users/1 or /users/1.json
+#   def update
+#     respond_to do |format|
+#       if @user.update(user_params)
+#         format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
+#         format.json { render :show, status: :ok, location: @user }
+#       else
+#         format.html { render :edit, status: :unprocessable_entity }
+#         format.json { render json: @user.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
+#
+#   # DELETE /users/1 or /users/1.json
+#   def destroy
+#     @user.destroy
+#
+#     respond_to do |format|
+#       format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+#       format.json { head :no_content }
+#     end
+#   end
+#
+#   private
+#     # Use callbacks to share common setup or constraints between actions.
+#     def set_user
+#       @user = User.find(params[:id])
+#     end
+#
+#     # Only allow a list of trusted parameters through.
+#     def user_params
+#       params.require(:user).permit(:name, :email)
+#     end
+# end
