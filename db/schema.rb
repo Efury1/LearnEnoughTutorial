184bf23f160f1c9d7e9c5185e808e8e563eb456e
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-24 10:51:02 +1000

    test driven development

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_20_071011) do
+ActiveRecord::Schema.define(version: 2022_01_24_004105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_01_20_071011) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -23,4 +23,34 @@ class UserTest < ActiveSupport::TestCase
     @user.name = "a" * 51
     assert_not @user.valid?
   end
+
+  test "email should not be too long" do
+    @users.email = "a" * 256
+    assert_not @user.valid?, "Address #{valid_address.inspect} should be valid"
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.CO A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+      foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each |invalid_addresses|
+      @user.email = invalid_addresses
+      assert_not @user.valid?, "#{invalid_addresses.inspect} should be invalid"
+    end
+  end
+
+  test "email address should be unquie" do
+    duplicate_user = @user.duplicate_user
+    duplicate_user = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
