5b8b22d22c167f27142fce85578330bbdbf07715
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-25 16:29:10 +1000

    Incorportate user authentication in application

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
-
-  validates :password, length: {minimum: 6}
-
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -10,6 +8,7 @@ class User < ApplicationRecord
     uniqueness: { case_sensitive: false }
 
   has_secure_password
+  validates :password, length: {minimum: 6}
   has_many :microposts
 
   # Returns the hash digest of the given string.
@@ -22,5 +21,16 @@ class User < ApplicationRecord
   #Returns a random token
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  #Remmbers a user in the database for us in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  #Returns true if the
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
