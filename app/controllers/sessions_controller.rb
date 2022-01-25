16c9372ff3625acc464cdb965a0cd99086e4b2a4
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-25 14:47:27 +1000

    Add temporary cookie to login session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,20 @@
-def new
- end
+class SessionsController < ApplicationController
 
- def create
-   user = User.find_by(email: params[:session][:email].downcase)
-   if user && user.authenticate(params[:session][:password])
-     # Log the user in and redirect to the user's show page.
-   else
-     flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
-     render 'new'
-   end
- end
+  def new
+  end
 
- def destroy
- end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
