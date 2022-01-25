5c63b5ecbb0eea3508a0b91a4905a2f3cefced5f
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-25 14:24:13 +1000

    Add user_login test and flash for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ def new
    if user && user.authenticate(params[:session][:password])
      # Log the user in and redirect to the user's show page.
    else
-     flash[:danger] = 'Invalid email/password combination' # Not quite right!
+     flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
      render 'new'
    end
  end
