ba21f9a83507ee115afa1e2938e6d8d90c0b4152
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-25 15:10:44 +1000

    Import bootstrap and java. Create dropdown menu for logined accounts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
+import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require turbolinks
+//= require_tree
