342119af114bc592353b265f4ae8afecb242a76b
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-25 13:50:49 +1000

    Create sessions controller and help. Fix style of signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root           'static_pages#home'
   get 'about'   => 'static_pages#about'
   get 'help'    => 'static_pages#help'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get 'login'   => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
