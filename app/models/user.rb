d70df1e727e41c64af2bcd136d7bba3a80a3b254
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-27 07:08:38 +1000

    Remember me functionalitu

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,7 @@ class User < ApplicationRecord
   end
   #Returns true if the
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
