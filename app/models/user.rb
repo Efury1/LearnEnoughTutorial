dcfce432986815bb51c060fca9229149b8fdc38c
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-27 16:24:58 +1000

    Implement session for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  
+
   has_many :microposts
 
   # Returns the hash digest of the given string.
@@ -27,6 +27,10 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def session_token
+    remember_digest || remember
   end
 
   # Forgets a user
