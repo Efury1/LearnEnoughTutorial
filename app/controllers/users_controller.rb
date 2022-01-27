be1fa18c7f733f8b21815f521516fc8802a0188b
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-27 18:14:33 +1000

    User update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
+    # @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -20,13 +21,18 @@ before_action :correct_user,   only: [:edit, :update]
       reset_session
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to root_url
     else
       render 'new'
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
