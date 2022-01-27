bb06512cb91f791c0d7a59bcf6cab4e288759ee6
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-28 07:44:50 +1000

    Add logo and change application name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ before_action :correct_user,   only: [:edit, :update]
     if @user.save
       reset_session
       log_in @user
-      flash[:success] = "Rate, add and explore new lunch places"
+      flash[:success] = "Welcome to Lunch Generator!"
       redirect_to root_url
     else
       render 'new'
