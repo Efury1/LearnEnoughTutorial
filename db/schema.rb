4368087ab920408fce519de9c14f7dcba18cfa00
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-31 08:50:22 +1000

    Add admin test and controls

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_211909) do
+ActiveRecord::Schema.define(version: 2022_01_30_214707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2022_01_27_211909) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,6 +6,39 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:fury)
   end
 
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              "password",
+                                            password_confirmation: "password",
+                                            admin: FILL_IN } }
+    assert_not @other_user.FILL_IN.admin?
+  end
+
+  test "should redirect destroy when not logged in " do
+    asset_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
+  end
   def "should redirect index when not logged in" do
     get :index
     assert_redirected_to login_url
