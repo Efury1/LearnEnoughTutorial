9c46e24312a46eefa088e4e5a833b04ead573450
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-01-31 16:49:10 +1000

    Sort password authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
-  attr_accessor :remember_token, :activation_token, :activation_digest, :reset_token
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
@@ -50,9 +58,8 @@ class User < ApplicationRecord
 
   # Activates an account.
   def activate
-    update_columns(activated: FILL_IN, activated_at: FILL_IN)
-    # update_attribute(:activated,    true)
-    # update_attribute(:activated_at, Time.zone.now)
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
   end
 
   # Sends activation email.
@@ -63,8 +70,9 @@ class User < ApplicationRecord
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: FILL_IN, reset_sent_at: FILL_IN)
+    # update_attribute(:reset_digest,  User.digest(reset_token))
+    # update_attribute(:reset_sent_at, Time.zone.now)
   end
 
   # Sends password reset email.
@@ -100,7 +108,7 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
 
-
+  private
 
     # Converts email to all lowercase.
     def downcase_email
