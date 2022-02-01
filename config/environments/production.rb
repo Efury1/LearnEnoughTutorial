2dc994c6153b26de8ea9da813599eccbf319c459
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-02-01 13:37:48 +1000

    Change production and storage to include images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  # Store uploaded files on Amazon AWS.
+  config.active_storage.service = :amazon
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
