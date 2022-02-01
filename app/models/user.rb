8dcb26ef5dbfbdaca028df162542473cbe0d8645
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2022-02-02 09:53:42 +1000

    Hotfix user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,10 +85,11 @@ class User < ApplicationRecord
 
   # Returns a user's status feed.
   def feed
-    following_ids = "SELECT followed_id FROM relationships
-                     WHERE  follower_id = :user_id"
-    Micropost.where("user_id IN (#{following_ids})
-                     OR user_id = :user_id", user_id: id)
+    Micropost.where("user_id = ?", id)
+    # following_ids = "SELECT followed_id FROM relationships
+    #                  WHERE  follower_id = :user_id"
+    # Micropost.where("user_id IN (#{following_ids})
+    #                  OR user_id = :user_id", user_id: id)
   end
 
   # Follows a user.
